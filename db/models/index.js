edaeeba82c8e3811d57c4e7f6073ea71b0474d5e
Yusuke Hirooka <yhirookajpn@gmail.com>
2021-07-19 23:39:07 +0900

    modified absolute path db/models

--- db/models/index.js
+++ db/models/index.js
@@ -41,7 +41,8 @@ sequelize = new Sequelize("cloth_to", "cloth_to_pro", "HY19940302hy", {
 
 // fs.readdirSync("./db/models/")
 // fs.readdirSync("./var/task/.next/serverless/db/models/")
-fs.readdirSync("./var/task/db/models/")
+// fs.readdirSync("./var/task/db/models/")
+fs.readdirSync(__dirname + "./db/models/")
   .filter((file) => {
     return (
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
