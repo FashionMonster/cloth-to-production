bd622fe7ba276d538ee44c8d162ec3550b9ce9b6
jpx7416 <Line!ca1>
2021-07-21 22:33:11 +0900

    test commit

--- db/models/index.js
+++ db/models/index.js
@@ -46,7 +46,7 @@ sequelize = new Sequelize("cloth_to", "cloth_to_pro", "HY19940302hy", {
 //fs.readdirSync(process.cwd() + "/db/models/" || __dirname)
 console.log("__filename" + __filename) // TODO 削除
 console.log("path.resolve(__dirname) > " + path.resolve(__dirname)) // TODO 削除
-fs.readdirSync(process.cwd()+"/.next")
+fs.readdirSync(process.cwd()+"/.next/serverless")
   .forEach((file) => {
   console.log("file > " + file) // TODO 削除
 //    return (
