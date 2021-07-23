31b1a873901b8a18b737860dc9d0c421283f5c59
Yusuke Hirooka <yhirookajpn@gmail.com>
2021-07-23 21:31:15 +0900

    modified model definition

--- db/models/useraccounts.js
+++ db/models/useraccounts.js
@@ -1,20 +1,7 @@
-const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
-  class UserAccounts extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      UserAccounts.hasMany(models.ContributionInfos, {
-        foreignKey: "user_id",
-        sourceKey: "user_id",
-      });
-    }
-  }
-  UserAccounts.init(
+/* eslint-disable import/no-anonymous-default-export */
+export default (sequelize, DataTypes) => {
+  const UserAccounts = sequelize.define(
+    "UserAccounts",
     {
       user_id: {
         type: DataTypes.STRING(255),
@@ -23,11 +10,14 @@ module.exports = (sequelize, DataTypes) => {
       user_name: DataTypes.STRING(20),
       group_id: DataTypes.STRING(255),
     },
-    {
-      sequelize,
-      modelName: "UserAccounts",
-      underscored: true,
-    }
+    { underscored: true }
   );
+  UserAccounts.associate = function (models) {
+    // associations can be defined here
+    UserAccounts.hasMany(models.ContributionInfos, {
+      foreignKey: "user_id",
+      sourceKey: "user_id",
+    });
+  };
   return UserAccounts;
 };
