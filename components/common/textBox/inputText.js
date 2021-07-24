2a1b036314c7d23fec9fa985b7e78357a2fe83fd
Yusuke Hirooka <yhirookajpn@gmail.com>
2021-07-24 12:57:04 +0900

    modified tailwind dynamic style

--- components/common/textBox/inputText.js
+++ components/common/textBox/inputText.js
@@ -3,9 +3,10 @@ const InputText = (props) => {
   // const divStyle = `w-${props.width} h-8`;
   // const inputStyle = `w-${props.width} h-8 border border-solid rounded-sm border-gray-400 disabled:bg-gray-100 disabled:text-black`;
 
-  const divStyle = "w-408 h-8";
+  const divStyle = props.width + " h-8";
   const inputStyle =
-    "w-408 h-8 border border-solid rounded-sm border-gray-400 disabled:bg-gray-100 disabled:text-black";
+    props.width +
+    " h-8 border border-solid rounded-sm border-gray-400 disabled:bg-gray-100 disabled:text-black";
 
   return (
     <div className={divStyle}>
